b0eda3c0c803c5eb22b92b241522cd52d5794660
couringtonj <jonathan.courington@outlook.com>
2025-10-09 13:47:26 -0400

    fgixesd value 5

--- 02.EnvironmentSetup/01.Project_template_modules/main.cpp
+++ 02.EnvironmentSetup/01.Project_template_modules/main.cpp
@@ -11,8 +11,9 @@ int main(){
     int value1 {10};
     int value2{20};
     int value3 {30};
-    int value4 {value1+value2+value3+value5};
     int value5 {50};
+    int value4 {value1+value2+value3+value5};
+    
     fmt::print("Hello the values are {} and {}!\n",value1,value2);
     fmt::println("Hello the values are {} and {} and {} and {} equals {}!",value1,value2,value3,value5,value4);
 }
