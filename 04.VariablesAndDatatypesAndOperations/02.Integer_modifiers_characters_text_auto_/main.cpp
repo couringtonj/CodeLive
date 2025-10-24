c1670a1445f6969b400718ad3a85b134ae8e028a
couringtonj <jonathan.courington@outlook.com>
2025-10-24 15:54:15 -0400

    multiple variable and collection types

--- 04.VariablesAndDatatypesAndOperations/02.Integer_modifiers_characters_text_auto_/main.cpp
+++ 04.VariablesAndDatatypesAndOperations/02.Integer_modifiers_characters_text_auto_/main.cpp
@@ -5,16 +5,32 @@
 */
 //This will comment out the entire line
 #include <fmt/format.h>
+#include<vector>
+#include<array>
+#include<string>
+
 import utilities;
 
 int main(){
-   //braced_initialization();
+//braced_initialization();
 
-    //functional_initialization();
+//functional_initialization();
 
-   //assignment_initialization();
+//assignment_initialization();
 
-  integer_modifiers();
+//integer_modifiers();
 
-    return 0;
+//floating_point();
+//Nan_infinity();
+
+//bool_variables();
+
+//char_and_text();
+ //collections();
+
+auto_numbers();
+
+
+
+
 }
